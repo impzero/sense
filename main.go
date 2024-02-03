bcb46e66f547caa5d434ccc478c1042b492ca954
Lyubo Lyubchev <lyubo_2317@abv.bg>
2024-02-03 23:01:12 +0100

    make "and" parser

--- main.go
+++ main.go
@@ -1,40 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/davecgh/go-spew/spew"
+)
 
 func main() {
 
 	source := "11111111222223333"
-	twoParser := lit('2')
-	fourParser := lit('4')
-
-	parser := or(twoParser, fourParser)
+	// orParser := or(lit('2'), lit('4'))
+	parser := and(lit('1'), lit('2'))
 	fmt.Println(parser(source))
 }
 
-type parser func(string) string
+type parser func(string) (string, error)
+
+func and(parser1, parser2 parser) parser {
+	return func(input string) (string, error) {
+		value1, err := parser1(input)
+		if err != nil {
+			return input, err
+		}
+		spew.Dump(value1)
+		value2, err := parser2(value1)
+		if err != nil {
+			return input, err
+		}
+		return value2, nil
+	}
+}
 
 func or(parser1, parser2 parser) parser {
-	return func(s string) string {
-		value1 := parser1(s)
-		value2 := parser2(s)
-
-		if value1 != s {
-			return value1
+	return func(input string) (string, error) {
+		value1, err := parser1(input)
+		if err == nil {
+			return value1, nil
 		}
-		return value2
+		value2, err := parser2(input)
+		if err == nil {
+			return value2, nil
+		}
+		return input, err
 	}
 }
 
 func lit(c rune) parser {
-	return func(input string) string {
-		found := 0
+	return func(input string) (string, error) {
 		for i, current := range input {
 			if current == c {
-				found = i
-				break
+				return input[i:], nil
 			}
 		}
-		return input[found:]
+		return input, errors.New("lit: could not find character")
 	}
 }
