80be428690230a7bf2e65e13964d79421715b26e
Lyubo Lyubchev <lyubo_2317@abv.bg>
2024-02-07 02:26:34 +0100

    make `seq` parser, document code

--- main.go
+++ main.go
@@ -6,12 +6,12 @@ import (
 )
 
 func main() {
-	source := "hello world!"
+	source := "ahello hello hello hello hello world!"
 	digits := "111122223333"
 
-	helloParser := parseString("hello")
-	helloAndSpaceParser := and(helloParser, parseChar(' '))
-	parsed, err := helloAndSpaceParser([]rune(source))
+	parser := many(any(parseChar('a'), and(parseString("hello"), parseChar(' '))))
+	parsed, err := parser([]rune(source))
+
 	manyOneParser := many(parseChar('1'))
 	parsedDigits, err := manyOneParser([]rune(digits))
 	if err != nil {
@@ -36,11 +36,14 @@ func (r result) String() string {
 	return fmt.Sprintf("Parsed: [%s], Remaining: [%s]", string(r.parsed), string(r.remaining))
 }
 
+// parser is what a parser should look like
 type parser func(input []rune) (result, error)
 
+// many tries to parse input until failiure
 func many(p1 parser) parser {
 	return func(input []rune) (result, error) {
 		aggregate := result{parsed: []rune{}, remaining: input}
+		var err error
 		for {
 			result1, err := p1(aggregate.remaining)
 			if err != nil {
@@ -49,10 +52,14 @@ func many(p1 parser) parser {
 			aggregate.remaining = result1.remaining
 			aggregate.parsed = append(aggregate.parsed, result1.parsed...)
 		}
+		if len(aggregate.parsed) == 0 {
+			return aggregate, err
+		}
 		return aggregate, nil
 	}
 }
 
+// and parses input first with p1 and then with p2
 func and(p1, p2 parser) parser {
 	return func(input []rune) (result, error) {
 		result1, err := p1(input)
@@ -68,6 +75,23 @@ func and(p1, p2 parser) parser {
 	}
 }
 
+// seq parses input following the sequence of parser from parsers
+func seq(parsers ...parser) parser {
+	return func(input []rune) (result, error) {
+		aggregate := result{parsed: []rune{}, remaining: input}
+		for _, p := range parsers {
+			r, err := p(aggregate.remaining)
+			if err != nil {
+				return result{parsed: []rune{}, remaining: input}, err
+			}
+			aggregate.remaining = r.remaining
+			aggregate.parsed = append(aggregate.parsed, r.parsed...)
+		}
+		return aggregate, nil
+	}
+}
+
+// any parses input with the first parser that suceeds from list
 func any(parsers ...parser) parser {
 	return func(input []rune) (result, error) {
 		for _, p := range parsers {
@@ -80,6 +104,7 @@ func any(parsers ...parser) parser {
 	}
 }
 
+// parseString parses input with matching string
 func parseString(s string) parser {
 	sRunes := []rune(s)
 	return func(input []rune) (result, error) {
@@ -95,6 +120,7 @@ func parseString(s string) parser {
 	}
 }
 
+// parseChar parses input with c
 func parseChar(c rune) parser {
 	return func(input []rune) (result, error) {
 		if c == input[0] {
